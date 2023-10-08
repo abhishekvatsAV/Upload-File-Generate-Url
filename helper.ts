538bb01270727d49ae9e9c9a7b184fe7a4a77e76
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2023-10-08 15:35:29 +0530

    added backend url

--- helper.ts
+++ helper.ts
@@ -1,2 +1,2 @@
 // export const backend_url = 'http://localhost:4000';
-export const backend_url = "https://uploadshort.onrender.com/";
+export const backend_url = "https://uploadshort.onrender.com";
